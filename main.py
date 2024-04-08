787d7ac17225e548df40a0c9e194b3e91f84b58c
Sardo <sardo.ip@sardo.work>
2024-04-08 23:58:45 +0900

    Able to download music from youtube now

--- main.py
+++ main.py
@@ -16,11 +16,18 @@ def musicList():
 @app.route('/music', methods = ['POST'])
 def uploadMusic():
     data = request.get_json()
-    music_type = data['type']
-    if (music_type == 'online'):
-        
-
-    return jsonify({"message": "Uploaded new message."}), 201
+    music_author = data['author']
+    music_source = data['source']
+    if (music_source == 'youtube'):
+        music_url = data['music_url']
+        music_info = music.downloadYoutubeMusic(music_url)
+        return jsonify({"message": "Uploaded new music.",
+            "music_title": music_info['music_title'],
+            "music_path": music_info['music_path'],
+            "music_author": music_author,
+            }), 201
+    else:
+        return jsonify({"message": "Uploaded new music."}), 201
 
 if __name__ == '__main__':
     app.run(debug = True)
--- music.py
+++ music.py
@@ -1,4 +1,13 @@
 import sqlite3
+import os
+from pydub import AudioSegment
+from pytube import YouTube
+
+class Music:
+   def __init__(self, id, title, author):
+      self.id = id
+      self.title = title
+      self.author = author
 
 def loadMusicList():
     conn = sqlite3.connect('sommie.db')
@@ -7,3 +16,25 @@ def loadMusicList():
     print(cur.fetchall())
     cur.close()
     conn.close()
+
+# Download Youtube video and convert it to mp3 file.
+def downloadYoutubeMusic(url):
+    yt = YouTube(url)
+    video = yt.streams.filter(only_audio=True).first()
+    destino = "saved_music"
+    out_file = video.download(output_path=destino)
+    base, ext = os.path.splitext(out_file)
+
+    audio = AudioSegment.from_file(out_file)
+    new_file = base + '.mp3'
+    audio.export(new_file, format='mp3')
+    #Remove old video file.
+    os.remove(out_file)
+
+    #return music info to main process.
+    music_info = {
+        "music_title": yt.title,
+        "music_path" : destino + base + '.mp3',
+    }
+
+    return music_info
